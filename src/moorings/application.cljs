e61573e389c8684cbbee5a20e9943f621f794020
Andrew Bruce <andrew@camelpunch.com>
2015-05-10 21:38:43 +0100

    Centre roughly on canal system I'm interested in

--- src/moorings/application.cljs
+++ src/moorings/application.cljs
@@ -1,7 +1,7 @@
 (ns moorings.application)
 
 (defn ^:export initialize []
-  (let [map-opts {:center { :lat -34.397 :lng 150.644}
+  (let [map-opts {:center { :lat 52.562995 :lng -1.323853}
                   :zoom 8}]
     (js/google.maps.Map. (.getElementById js/document "map-canvas")
                          (clj->js map-opts)) ))
