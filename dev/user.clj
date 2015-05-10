398f5e472626ada3972b2d89017b0a6a75ee0deb
Andrew Bruce <andrew@camelpunch.com>
2015-05-10 21:05:51 +0100

    Can pass opts to js builder

--- dev/user.clj
+++ dev/user.clj
@@ -5,15 +5,17 @@
 
 (def js-dir "resources/public/js")
 
-(defn build []
+(defn build [& [opts]]
   (delete-file-recursively js-dir :silently)
   (js/build "src"
-            {:main 'moorings.application
-             :output-to (str js-dir "/application.js")
-             :output-dir js-dir
-             :asset-path "js"}))
+            (merge {:main 'moorings.application
+                    :output-to (str js-dir "/application.js")
+                    :output-dir js-dir
+                    :asset-path "js"}
+                   (or opts {}))))
 
 (comment
   (refresh)
   (build)
+  (build {:optimizations :advanced})
   )
